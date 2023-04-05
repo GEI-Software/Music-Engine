b0c7fb8377594a0a6ed66baba72d8dcf510c9cac
pautorne <paute2001@gmail.com>
2023-04-05 15:48:26 +0200

    some models

--- Django/Django_Music_Engine/music_engine/models.py
+++ Django/Django_Music_Engine/music_engine/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MusicalMaterial(models.Model):
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    type = models.CharField(choices=('ENSAIG', 'ENREGISTRAR'))
+
+    # incorporated_material = models.ForeignKey(MusicalMaterial, on_delete=CASCADE)
+    # compatible_material = models.ForeignKey(MusicalMaterial, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class MusicalStudio(models.Model):
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    type = models.CharField(choices=('ENSAIG', 'ENREGISTRAR'))
+    
+    incorporated_material = models.ForeignKey(MusicalMaterial, on_delete=models.CASCADE)
+
+    # compatible_material = models.ForeignKey(MusicalMaterial, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
