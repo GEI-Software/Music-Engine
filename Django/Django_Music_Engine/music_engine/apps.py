dad32877c5477ba52838ec440b1b0f3c61e23512
pautorne <paute2001@gmail.com>
2023-05-25 10:28:58 +0200

    Automatització creació factures, aspectes de disseny

--- Django/Django_Music_Engine/music_engine/apps.py
+++ Django/Django_Music_Engine/music_engine/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class MusicEngineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'music_engine'
+
+    def ready(self):
+        import django.core.signals
